1b745a7e7f8c492431e0b75e2264f41c145ef814
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-07-19 19:59:05 -0700

    formatting updates; added movie detail logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,11 +26,11 @@ function searchTitle() {
     //clear current movie list and result count
     $("#summary-section").empty();
     $("#result-count").empty();
-    
+    $("#review-section").empty();
     
     //configure search URL
     var searchTerm = $("#title-input").val().trim().replace(/ /g, "+");   
-    var queryURL = searchURL + "?api_key=" + apiKey + "&query=" + searchTerm;
+    var queryURL = searchURL + "?api_key=" + apiKey + "&query=" + searchTerm + "&include_adult=false";
     
     //generate API call and retrieve movie list
     $.ajax({
@@ -39,7 +39,7 @@ function searchTitle() {
     }).then(function (response) {
         numOfResults = response.total_results;
         movieList = response.results;
-        
+        console.log(movieList);
         //display number of results
         var resultCount = $("#result-count");
         var countLabel = $("<span>").addClass("count-label").html("Results: ");
@@ -59,8 +59,9 @@ function getDetails(movieID) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
         movieDetails = response;
+        //display details
+        renderDetails();
     });
 }
 
@@ -76,7 +77,7 @@ function renderMovieList() {
         var vote = movieList[i].vote_average;
 
         //create HTML elements
-        var movieRow = $("<div>").addClass("row mb-3 movie-row");
+        var movieRow = $("<div>").addClass("movie-row").attr("data-movie-id", movieID);
         var movieImg = $("<img>").attr("src", imgBaseURL + imgPath);
         var movieInfo = $("<div>").addClass("movie-info");
         var movieTitle = $("<div>").addClass("movie-title").html(title);
@@ -93,89 +94,47 @@ function renderMovieList() {
         movieInfo.append(movieVote);
         movieDate.prepend(dateLabel);
         movieVote.prepend(voteLabel);
-        console.log(movieRow);
         $("#summary-section").append(movieRow);
-
     }
-
 }
 
 //render movie details
-
+function renderDetails() {
+    console.log(movieDetails);
+    var reviewSection = $("#review-section");
+    reviewSection.empty();
+    var tagline = $("<div>").addClass("tagline").html(movieDetails.tagline);
+    var summary = $("<div>").addClass("summary").html(movieDetails.overview);
+    reviewSection.append(tagline).append(summary);
+    $("#review-section").css("opacity", "1");
+}
 
 
 $("#title-search-btn").on("click", searchTitle);
 
 //when movie row is clicked update css and display reviews
 $(document).on("click", ".movie-row", function () {
-    if ($(this).attr("data-state") === "selected") {
+    if ($(this).attr("id") === "selected-row") {
         //reset css for all rows
         $(".movie-row").css("opacity", "1");
         $(this).css("border-color", "#ddd");
-        //fade out reviews
-        anime({
-            targets: "#review-section",
-            opacity: 0,
-            duration: 1000,
-            easing: "linear"
-          });
+        //fade reviews
+        $("#review-section").css("opacity", "0");
         //set state
-        $(".movie-row").attr("data-state", "not-selected");
+        $(".movie-row").removeAttr("id");
     }
     else {
-        //fade out reviews
-        anime({
-            targets: "#review-section",
-            opacity: 0,
-            duration: 1000,
-            easing: "linear"
-          });
         //fade non-selected rows
         $(".movie-row").css("opacity", "0.5");
         $(".movie-row").css("border-color", "#ddd");
         //bring selected row into focus
         $(this).css("opacity", "1");
         $(this).css("border-color", "#000");
-
-        //construct NTY review elements
-        $("#review-section").empty();
-        var nytReview = $("<div>");
-        nytReview.addClass("row mb-3 px-3 text-justify");
-        nytReview.attr("id", "nyt-review");
-        var nytTitle = $("<h4>");
-        nytTitle.addClass("mx-auto text-center review-title");
-        nytTitle.html("New York Times Review");
-        var nytText = $("<p>");
-        nytText.html("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam eaque iure suscipit saepe nihil minus! Laborum, culpa consequuntur laudantium laboriosam unde neque tempora quis iusto officiis consectetur quibusdam dicta?");
-        nytReview.append(nytTitle);
-        nytReview.append(nytText);
-
-        //construct USA review elements
-        var usaReview = $("<div>");
-        usaReview.addClass("row mb-3 px-3 text-justify");
-        usaReview.attr("id", "usa-review");
-        var usaTitle = $("<h4>");
-        usaTitle.addClass("mx-auto text-center review-title");
-        usaTitle.html("USA Today Review");
-        var usaText = $("<p>");
-        usaText.html("Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quibusdam voluptatum inventore consequuntur velit amet molestias quisquam, aperiam veniam obcaecati vero. Dolore unde sed accusamus exercitationem hic repellat iusto quae.");
-        usaReview.append(usaTitle);
-        usaReview.append(usaText);
-
-        //add elements to page
-        $("#review-section").append(nytReview);
-        $("#review-section").append(usaReview);
         //set row state
-        $(".movie-row").attr("data-state", "not-selected");
-        $(this).attr("data-state", "selected");
-        //animiate fade in
-            anime({
-                targets: "#review-section",
-                opacity: 1,
-                duration: 1500,
-                easing: "linear",
-                offset: "+=1000"
-              });  
+        $(".movie-row").removeAttr("id");
+        $(this).attr("id", "selected-row");
+        //get movie details
+        getDetails($(this).attr("data-movie-id"));
     }
 
 })
