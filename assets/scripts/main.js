bcf7acf901b9455c4fa2624e4c5ac6ecdb5368ba
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-07-19 18:15:27 -0700

    added TMDb API and search functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,135 @@
+//animate logo
+anime({
+    targets: ".jumbotron",
+    translateY: ["-250px", "0px"],
+    duration: "2000"
+})
+//animate search bars
+anime({
+    targets: ".search-inputs",
+    translateX: ["-500px", "0px"],
+    duration: "2000"    
+})
+
+//TMDb API config
+var searchURL = "https://api.themoviedb.org/3/search/movie"
+var movieURL = "https://api.themoviedb.org/3/movie/"
+var apiKey = "e48a4ae3c093a2322becafcc6dc5c8a0";
+
+//define global variables
+var numOfResults = 0;
+var movieList = [];
+var movieDetails = {};
+
+//search for movies by title
+function searchTitle() {
+    //clear current movie list and result count
+    $("#summary-section").empty();
+    $("#result-count").empty();
+    
+    
+    //configure search URL
+    var searchTerm = $("#title-input").val().trim().replace(/ /g, "+");   
+    var queryURL = searchURL + "?api_key=" + apiKey + "&query=" + searchTerm;
+    
+    //generate API call and retrieve movie list
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        numOfResults = response.total_results;
+        movieList = response.results;
+        
+        //display number of results
+        var resultCount = $("#result-count");
+        var countLabel = $("<span>").addClass("count-label").html("Results: ");
+        resultCount.html(numOfResults);
+        resultCount.prepend(countLabel);
+        
+        //render list of movies
+        renderMovieList();
+    });
+}
+
+//get movie details
+function getDetails(movieID) {
+    var queryURL = movieURL + movieID + "?api_key=" + apiKey
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        movieDetails = response;
+    });
+}
+
+//render movie list
+function renderMovieList() {
+    for (let i = 0; i < movieList.length && i < 10; i++) {
+        //assign movie values
+        var imgBaseURL = "https://image.tmdb.org/t/p/w200";
+        var movieID = movieList[i].id;
+        var title = movieList[i].title;
+        var releaseDate = movieList[i].release_date;
+        var imgPath = movieList[i].poster_path;
+        var vote = movieList[i].vote_average;
+
+        //create HTML elements
+        var movieRow = $("<div>").addClass("row mb-3 movie-row");
+        var movieImg = $("<img>").attr("src", imgBaseURL + imgPath);
+        var movieInfo = $("<div>").addClass("movie-info");
+        var movieTitle = $("<div>").addClass("movie-title").html(title);
+        var movieDate = $("<div>").addClass("release-date").html(releaseDate);
+        var movieVote = $("<div>").addClass("avg-vote").html(vote);
+        var dateLabel = $("<span>").addClass("label").html("Release Date: ");
+        var voteLabel = $("<span>").addClass("label").html("Average Vote: ");
+        
+        //add elements to page
+        movieRow.append(movieImg);
+        movieRow.append(movieInfo);
+        movieInfo.append(movieTitle);
+        movieInfo.append(movieDate);
+        movieInfo.append(movieVote);
+        movieDate.prepend(dateLabel);
+        movieVote.prepend(voteLabel);
+        console.log(movieRow);
+        $("#summary-section").append(movieRow);
+
+    }
+
+}
+
+//render movie details
+
+
+
+$("#title-search-btn").on("click", searchTitle);
+
 //when movie row is clicked update css and display reviews
 $(document).on("click", ".movie-row", function () {
-    if ($(this).attr("data-state") === "true") {
+    if ($(this).attr("data-state") === "selected") {
         //reset css for all rows
         $(".movie-row").css("opacity", "1");
         $(this).css("border-color", "#ddd");
-        //remove reviews
-        $("#review-section").empty();
+        //fade out reviews
+        anime({
+            targets: "#review-section",
+            opacity: 0,
+            duration: 1000,
+            easing: "linear"
+          });
         //set state
-        $(".movie-row").attr("data-state", "false");
+        $(".movie-row").attr("data-state", "not-selected");
     }
     else {
+        //fade out reviews
+        anime({
+            targets: "#review-section",
+            opacity: 0,
+            duration: 1000,
+            easing: "linear"
+          });
         //fade non-selected rows
         $(".movie-row").css("opacity", "0.5");
         $(".movie-row").css("border-color", "#ddd");
@@ -43,14 +163,19 @@ $(document).on("click", ".movie-row", function () {
         usaReview.append(usaText);
 
         //add elements to page
-        setTimeout(function() {
-            $("#review-section").append(nytReview);
-            $("#review-section").append(usaReview);
-        }, 500);
-
+        $("#review-section").append(nytReview);
+        $("#review-section").append(usaReview);
         //set row state
-        $(".movie-row").attr("data-state", "false");
-        $(this).attr("data-state", "true");
+        $(".movie-row").attr("data-state", "not-selected");
+        $(this).attr("data-state", "selected");
+        //animiate fade in
+            anime({
+                targets: "#review-section",
+                opacity: 1,
+                duration: 1500,
+                easing: "linear",
+                offset: "+=1000"
+              });  
     }
 
 })
